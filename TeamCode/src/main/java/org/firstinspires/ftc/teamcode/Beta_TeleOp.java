841825f3b1da9e224054643a98794ff1b1aaea27
Aiden Gilmore <91899915+DominantDuck@users.noreply.github.com>
2023-10-14 20:35:06 -0400

    updated ports

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Beta_TeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Beta_TeleOp.java
@@ -13,19 +13,17 @@ public class Beta_TeleOp extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
 
-        DcMotor rightFrontMotor = hardwareMap.get(DcMotor.class,"rightFrontMotor");
-        DcMotor leftFrontMotor = hardwareMap.get(DcMotor.class,"leftFrontMotor");
-        DcMotor rightRearMotor = hardwareMap.get(DcMotor.class,"rightRearMotor");
-        DcMotor leftRearMotor = hardwareMap.get(DcMotor.class,"leftRearMotor");
+        DcMotor rightFrontMotor = hardwareMap.get(DcMotor.class,"rightFront");
+        DcMotor leftFrontMotor = hardwareMap.get(DcMotor.class,"leftFront");
+        DcMotor rightRearMotor = hardwareMap.get(DcMotor.class,"rightRear");
+        DcMotor leftRearMotor = hardwareMap.get(DcMotor.class,"leftRear");
+        DcMotor leftSpin = hardwareMap.get(DcMotor.class,"leftSpin");
+        DcMotor rightSpin = hardwareMap.get(DcMotor.class,"rightSpin");
+        DcMotor leftArm = hardwareMap.get(DcMotor.class,"leftArm");
+        DcMotor rightArm = hardwareMap.get(DcMotor.class,"rightArm");
 
-        DcMotorEx armExtension = hardwareMap.get(DcMotorEx.class, "armExtension");
-        Servo gripServo = hardwareMap.get(Servo.class,"gripServo");
-
-        RevBlinkinLedDriver ledLights = hardwareMap.get(RevBlinkinLedDriver.class,"ledLights");
         double speed;
-        armExtension.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         waitForStart();
-        armExtension.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         while (opModeIsActive()) {
             //Speed Control
             if (gamepad1.right_bumper) {
@@ -39,59 +37,21 @@ public class Beta_TeleOp extends LinearOpMode {
             //Controller Input - Moves Drive Train
             double vertical = gamepad1.left_stick_y * speed;
             double horizontal = gamepad1.left_stick_x * speed;
-            double pivot = -gamepad1.right_stick_x * speed; //normal has negative
+            double pivot = -gamepad1.right_stick_y * speed; //normal has negative
 
             rightFrontMotor.setPower(-pivot + vertical + horizontal);
             leftFrontMotor.setPower(-pivot - vertical + horizontal);
             rightRearMotor.setPower(-pivot + vertical - horizontal);
             leftRearMotor.setPower(-pivot - vertical - horizontal);
 
-//            Arm Extension
-            double value = gamepad2.right_stick_y;
-
-            if(armExtension.getCurrentPosition() > 100) {
-                if(value >= 0) {
-                    armExtension.setPower((value * 0.5) + 0.1);
-                } else {
-                    armExtension.setPower((value * 0.2) + 0.1);
-                }
-            } else if(armExtension.getCurrentPosition() > -100){
-                if(value >= 0) {
-                    armExtension.setPower((value * 0.5));
-                } else {
-                    armExtension.setPower((value * 0.2));
-                }
-            } else {
-                armExtension.setPower(0.6);
-            }
-
-            if(gamepad2.x) {
-                gripServo.setPosition(0.15);
-            }
-            if(gamepad2.b) {
-                gripServo.setPosition(0);
-            }
-
-            //LED Lights - Utilizes
-            if (armExtension.getCurrentPosition() > 150) {
-                ledLights.setPattern(RevBlinkinLedDriver.BlinkinPattern.RED);
-            } else if (armExtension.getCurrentPosition() < 150 && armExtension.getCurrentPosition() > -75) {
-                ledLights.setPattern(RevBlinkinLedDriver.BlinkinPattern.GREEN);
-            } else
-                ledLights.setPattern(RevBlinkinLedDriver.BlinkinPattern.STROBE_GOLD);
-
-            telemetry.addData("Position: ",armExtension.getCurrentPosition());
-            telemetry.addData("Servo Position: ",gripServo.getPosition());
-            telemetry.addData("Target: ",armExtension.getTargetPosition());
-            telemetry.addData("Power: ",armExtension.getPower());
-            telemetry.update();
+            leftArm.setPower(gamepad2.left_stick_y);
+            rightArm.setPower(gamepad2.left_stick_y);
         }
 
         rightFrontMotor.setPower(0.0);
         leftFrontMotor.setPower(0.0);
         rightRearMotor.setPower(0.0);
         leftRearMotor.setPower(0.0);
-        armExtension.setPower(0.0);
 
     }
 
