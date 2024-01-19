cba6d24439e8ab7371d0f5e7d6b8dce3436e2b29
Aiden Gilmore <91899915+DominantDuck@users.noreply.github.com>
2024-01-19 08:21:01 -0500

    update led

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DistanceTesting.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DistanceTesting.java
@@ -1,5 +1,6 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.hardware.rev.RevBlinkinLedDriver;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DistanceSensor;
@@ -13,11 +14,20 @@ public class DistanceTesting extends LinearOpMode {
     public void runOpMode() throws InterruptedException {
         DistanceSensor distanceSensor = hardwareMap.get(DistanceSensor.class, "distanceSensor");
 
+        RevBlinkinLedDriver ledLights = hardwareMap.get(RevBlinkinLedDriver.class,"ledLights");
+
         waitForStart();
 
         while (opModeIsActive()) {
             telemetry.addData("Distance: ", distanceSensor.getDistance(DistanceUnit.INCH));
             telemetry.update();
+
+            if (distanceSensor.getDistance(DistanceUnit.INCH) < 8) {
+                ledLights.setPattern(RevBlinkinLedDriver.BlinkinPattern.RED);
+            } else if (distanceSensor.getDistance(DistanceUnit.INCH) > 8.01 && distanceSensor.getDistance(DistanceUnit.INCH) < 10 ) {
+                ledLights.setPattern(RevBlinkinLedDriver.BlinkinPattern.YELLOW);
+            } else
+                ledLights.setPattern(RevBlinkinLedDriver.BlinkinPattern.GREEN);
         }
     }
 }
